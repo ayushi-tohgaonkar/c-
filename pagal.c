f9a3c55510d7832cdab9aa0c8d69ecd409557d10
ayushi-tohgaonkar <ayushitohgaonkar02@gmail.com>
2020-04-29 20:05:21 +0530

    my firt version

---
+++ pagal.c
@@ -0,0 +1,4 @@
+nachiket is mad bcz he loves ashwini and she is also intrested in him.
+but unhe duniya ka dar h.
+ye jalim duniya unhe ek nai hone de rai h
+mera pyara dost plz koi uske liye kch kro......
